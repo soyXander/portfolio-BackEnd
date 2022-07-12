5505c7ad19f68e491a8b79b35aedb085ce6a2fc6
Xander.- <zn.xander@gmail.com>
2022-07-11 23:12:00 -0300

    Actualización CRUD Skills

--- src/main/java/com/argentinaprograma/backend/controller/SkillController.java
+++ src/main/java/com/argentinaprograma/backend/controller/SkillController.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,11 +31,11 @@ public class SkillController {
 		return new ResponseEntity(skillService.list(), HttpStatus.OK);
 	}
 
-	@PostMapping("/agregar")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PostMapping("/guardar")
 	public ResponseEntity<?> save(@RequestBody SkillDTO skill) {
 		if (StringUtils.isBlank(skill.getSkill()))
 			return new ResponseEntity(new Message("La habilidad es obligatoria"), HttpStatus.BAD_REQUEST);
-
 		if (skill.getPercentage() < 0 || skill.getPercentage() > 100)
 			return new ResponseEntity(new Message("El porcentaje debe ser entre 0 y 100"), HttpStatus.BAD_REQUEST);
 
@@ -46,19 +47,20 @@ public class SkillController {
 		return new ResponseEntity(new Message("Habilidad agregada"), HttpStatus.CREATED);
 	}
 
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@GetMapping("/detalle/{id}")
 	public ResponseEntity<Skill> getById(@PathVariable("id") Long id){
 		if(!skillService.existsById(id))
-			return new ResponseEntity(new Message("No existe"), HttpStatus.NOT_FOUND);
+			return new ResponseEntity(new Message("Habilidad no encontrada"), HttpStatus.NOT_FOUND);
 		Skill skill = skillService.getOne(id).get();
 		return new ResponseEntity(skill, HttpStatus.OK);
 	}
 
-	@PutMapping("/editar/{id}")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PutMapping("/actualizar/{id}")
 	public ResponseEntity<?> update(@PathVariable Long id, @RequestBody SkillDTO skillDTO) {
 		if (StringUtils.isBlank(skillDTO.getSkill()))
 			return new ResponseEntity(new Message("La habilidad es obligatoria"), HttpStatus.BAD_REQUEST);
-
 		if (skillDTO.getPercentage() <= 0 || skillDTO.getPercentage() >= 100)
 			return new ResponseEntity(new Message("El porcentaje debe ser entre 0 y 100"), HttpStatus.BAD_REQUEST);
 
@@ -70,10 +72,11 @@ public class SkillController {
 		return new ResponseEntity(new Message("Habilidad actualizada"), HttpStatus.CREATED);
 	}
 
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@DeleteMapping("/eliminar/{id}")
 	public ResponseEntity<?> delete(@PathVariable Long id){
 		if(!skillService.existsById(id))
-			return new ResponseEntity(new Message("No existe"), HttpStatus.NOT_FOUND);
+			return new ResponseEntity(new Message("Habilidad no encontrada"), HttpStatus.NOT_FOUND);
 
 		skillService.delete(id);
 		return new ResponseEntity(new Message("Habilidad eliminada"), HttpStatus.OK);
